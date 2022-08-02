004122127c4c965c027387e164299cab12733751
anik <anikmishra836@gmail.com>
2022-08-02 14:19:37 +0530

    updating

--- src/Recursion/index.js
+++ src/Recursion/index.js
@@ -6,13 +6,16 @@ import TravelingVehiclesTypes from '../ObjectData/index';
  * @param {object} object
  * @returns node
  */
-const Transform = (object) =>
-  Object.entries(object).map(([key, value]) => ({
-    key,
-    ...(value && typeof value === 'object'
-      ? { value: '', children: Transform(value) }
-      : { value, children: [] }),
-  }));
+const Transform = (object) => {
+  return Object.entries(object).map(([key, value]) => {
+    return {
+      key,
+      ...(value && typeof value === 'object'
+        ? { value: '', children: Transform(value) }
+        : { value, children: [] }),
+    };
+  });
+};
 // calling function
 const result = Transform(TravelingVehiclesTypes);
 console.log(result);
